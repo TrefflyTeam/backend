bea5c281a2e9b0c1c9f8af5627322f094b0bb4d6
unknown <mah235715@gmail.com>
2025-02-21 18:57:38 +0300

    test

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,8 @@ FROM golang:1.23-alpine
 
 WORKDIR /app
 
-COPY go.mod go.sum ./
-
-RUN go mod download
-
 COPY . .
 
 RUN go build -o main .
-
-EXPOSE 8080
 
 ENTRYPOINT ["./main"]
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import _ "github.com/jackc/pgx/v5"
+import "github.com/gin-gonic/gin"
 
 func main() {
-
+	router := gin.Default()
+	err := router.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
 }
