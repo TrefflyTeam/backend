68e678681c26f16fe8dd01b3e4453aab121d6c0c
unknown <mah235715@gmail.com>
2025-02-22 17:10:55 +0300

    test endpoint

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	router := gin.Default()
+	router.GET("/test", func(c *gin.Context) { c.JSON(200, gin.H{"hello": "world"}) })
 	err := router.Run(":8080")
 	if err != nil {
 		panic(err)
