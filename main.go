8b19148ed38d0c31d7b20376d6e5838f52b8f514
unknown <mah235715@gmail.com>
2025-03-22 16:23:54 +0300

    feat: add base HTTP server

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"database/sql"
+	_ "github.com/jackc/pgx/v5/stdlib"
+	"log"
+	"treffly/api"
+	db "treffly/db/sqlc"
+)
 
+const (
+	dbDriver      = "pgx"
+	dbSource      = "postgresql://postgres@localhost:5432/postgres?sslmode=disable&password=balls"
+	serverAddress = "localhost:8080"
+)
+
+func main() {
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db")
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
