fe86c04515ca168eb460a859feffb7581ee26651
m1ll3r1337 <mah235715@gmail.com>
2025-03-23 22:44:46 +0300

    feat: add PASETO token maker to the server

--- api/server.go
+++ api/server.go
@@ -1,23 +1,36 @@
 package api
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	db "treffly/db/sqlc"
+	"treffly/token"
+	"treffly/util"
 )
 
 type Server struct {
-	store db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %w", err)
+	}
+	server := &Server{
+		store:      store,
+		tokenMaker: tokenMaker,
+		config:     config,
+	}
 	router := gin.Default()
 
 	router.POST("/users", server.createUser)
 
 	server.router = router
-	return server
+	return server, nil
 }
 
 func (server *Server) Start(address string) error {
