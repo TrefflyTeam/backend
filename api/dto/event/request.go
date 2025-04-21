92997b24ad8c6b60e252024fbbd1b0b938d46e2e
m1ll3r1337 <mah235715@gmail.com>
2025-04-21 16:53:24 +0300

    refactor: add suffixes to event dtos

--- api/dto/event/request.go
+++ api/dto/event/request.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-type CreateEvent struct {
+type CreateEventRequest struct {
 	Name        string         `json:"name" binding:"required,event_name,min=5,max=50"`
 	Description string         `json:"description" binding:"required,min=50,max=1000"`
 	Capacity    int32          `json:"capacity" binding:"required,min=1,max=500"`
@@ -17,7 +17,7 @@ type CreateEvent struct {
 	Tags        []int32        `json:"tags" binding:"required,min=1,max=3,dive,required,positive"`
 }
 
-type UpdateEvent struct {
+type UpdateEventRequest struct {
 	Name        string         `json:"name" binding:"required,event_name,min=5,max=50"`
 	Description string         `json:"description" binding:"required,min=50,max=1000"`
 	Capacity    int32          `json:"capacity" binding:"required,min=1,max=500"`
