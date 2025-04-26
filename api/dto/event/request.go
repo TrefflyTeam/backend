c6317c156b6b2ad7f770361d562a621d4a0ba75e
m1ll3r1337 <mah235715@gmail.com>
2025-04-26 18:32:29 +0300

    refactor: change dto annotation on form data requests

--- api/dto/event/request.go
+++ api/dto/event/request.go
@@ -6,26 +6,26 @@ import (
 )
 
 type CreateEventRequest struct {
-	Name        string         `json:"name" binding:"required,event_name,min=5,max=50"`
-	Description string         `json:"description" binding:"required,min=50,max=1000"`
-	Capacity    int32          `json:"capacity" binding:"required,min=1,max=500"`
-	Latitude    pgtype.Numeric `json:"latitude" binding:"required,latitude"`
-	Longitude   pgtype.Numeric `json:"longitude" binding:"required,longitude"`
-	Address     string         `json:"address" binding:"required"`
-	Date        time.Time      `json:"date" binding:"required,valid_date"`
-	IsPrivate   bool           `json:"is_private" binding:"boolean"`
-	Tags        []int32        `json:"tags" binding:"required,min=1,max=3,dive,required,positive"`
+	Name        string         `form:"name" binding:"required,event_name,min=5,max=50"`
+	Description string         `form:"description" binding:"required,min=50,max=1000"`
+	Capacity    int32          `form:"capacity" binding:"required,min=1,max=500"`
+	Latitude    pgtype.Numeric `form:"latitude" binding:"required,latitude"`
+	Longitude   pgtype.Numeric `form:"longitude" binding:"required,longitude"`
+	Address     string         `form:"address" binding:"required"`
+	Date        time.Time      `form:"date" binding:"required,valid_date"`
+	IsPrivate   bool           `form:"is_private" binding:"boolean"`
+	Tags        []int32        `form:"tags" binding:"required,min=1,max=3,dive,required,positive"`
 }
 
 type UpdateEventRequest struct {
-	Name        string         `json:"name" binding:"required,event_name,min=5,max=50"`
-	Description string         `json:"description" binding:"required,min=50,max=1000"`
-	Capacity    int32          `json:"capacity" binding:"required,min=1,max=500"`
-	Latitude    pgtype.Numeric `json:"latitude" binding:"required,latitude"`
-	Longitude   pgtype.Numeric `json:"longitude" binding:"required,longitude"`
-	Address     string         `json:"address" binding:"required"`
-	Date        time.Time      `json:"date" binding:"required,valid_date"`
-	IsPrivate   bool           `json:"is_private" binding:"boolean"`
-	Tags        []int32        `json:"tags" binding:"required,min=1,max=3,dive,required,positive"`
-	DeleteImage bool           `json:"delete_image" binding:"boolean"`
+	Name        string         `form:"name" binding:"required,event_name,min=5,max=50"`
+	Description string         `form:"description" binding:"required,min=50,max=1000"`
+	Capacity    int32          `form:"capacity" binding:"required,min=1,max=500"`
+	Latitude    pgtype.Numeric `form:"latitude" binding:"required,latitude"`
+	Longitude   pgtype.Numeric `form:"longitude" binding:"required,longitude"`
+	Address     string         `form:"address" binding:"required"`
+	Date        time.Time      `form:"date" binding:"required,valid_date"`
+	IsPrivate   bool           `form:"is_private" binding:"boolean"`
+	Tags        []int32        `form:"tags" binding:"required,min=1,max=3,dive,required,positive"`
+	DeleteImage bool           `form:"delete_image" binding:"boolean"`
 }
--- api/dto/user/request.go
+++ api/dto/user/request.go
@@ -12,8 +12,8 @@ type LoginRequest struct {
 }
 
 type UpdateUserRequest struct {
-	Username string `json:"username" binding:"required,username,min=2,max=20"`
-	DeleteImage bool `json:"delete_image" binding:"boolean"`
+	Username string `form:"username" binding:"required,username,min=2,max=20"`
+	DeleteImage bool `form:"delete_image" binding:"boolean"`
 }
 
 type UpdateCurrentUserTagsRequest struct {
