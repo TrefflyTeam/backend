c4c891b3f171b1eccb0c24f146f62c69906ad561
m1ll3r1337 <mah235715@gmail.com>
2025-05-19 12:37:07 +0300

    feat: add password reset service

--- api/service/user/service.go
+++ api/service/user/service.go
@@ -2,9 +2,16 @@ package userservice
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/binary"
+	"errors"
+	"fmt"
 	"github.com/google/uuid"
+	"math"
+	"strconv"
 	"treffly/api/models"
 	"treffly/apperror"
+	"treffly/db/redis"
 	"treffly/db/sqlc"
 	"treffly/token"
 	"treffly/util"
@@ -12,15 +19,17 @@ import (
 
 type Service struct {
 	store      db.Store
+	resetStore redis.ResetStore
 	tokenMaker token.Maker
 	config     util.Config
 }
 
-func New(store db.Store, tokenMaker token.Maker, config util.Config) *Service {
+func New(store db.Store, redisStore redis.ResetStore, tokenMaker token.Maker, config util.Config) *Service {
 	return &Service{
 		store:      store,
 		tokenMaker: tokenMaker,
 		config:     config,
+		resetStore: redisStore,
 	}
 }
 
@@ -124,11 +133,11 @@ func (s *Service) UpdateUser(ctx context.Context, params models.UpdateUserParams
 	}
 
 	arg := db.UpdateUserTxParams{
-		UserID:       params.ID,
-		Username:     params.Username,
-		NewImageID:   imageID,
-		NewPath:      path,
-		OldImageID:   user.ImageID.Bytes,
+		UserID:     params.ID,
+		Username:   params.Username,
+		NewImageID: imageID,
+		NewPath:    path,
+		OldImageID: user.ImageID.Bytes,
 	}
 
 	updatedUser, err := s.store.UpdateUserTx(ctx, arg)
@@ -150,4 +159,113 @@ func (s *Service) UpdateUserTags(ctx context.Context, params models.UpdateUserTa
 
 func (s *Service) DeleteUser(ctx context.Context, userID int32) error {
 	return s.store.DeleteUser(ctx, userID)
+}
+
+func (s *Service) InitiatePasswordReset(ctx context.Context, email string) (string, error) {
+	user, err := s.store.GetUserByEmail(ctx, email)
+	if err != nil {
+		return "", nil
+	}
+
+	code, err := s.generateResetCode()
+	if err != nil {
+		return "", err
+	}
+
+	err = s.resetStore.SaveCode(ctx, string(user.ID), code, s.config.ResetCodeTTL)
+	if err != nil {
+		return "", err
+	}
+
+	return code, nil
+}
+
+func (s *Service) ConfirmResetCode(ctx context.Context, email, code string) (string, error) {
+	user, err := s.store.GetUserByEmail(ctx, email)
+	if err != nil {
+		return "", nil
+	}
+
+	storedCode, err := s.resetStore.GetCode(ctx, string(user.ID))
+	if err != nil {
+		return "", err
+	}
+
+	if storedCode != code {
+		return "", errors.New("invalid code")
+	}
+
+	t, _, err := s.tokenMaker.CreateToken(user.ID, s.config.ResetTokenDuration)
+	if err != nil {
+		return "", err
+	}
+
+	id := strconv.Itoa(int(user.ID))
+
+	if err := s.resetStore.SaveToken(ctx, t, id, s.config.ResetTokenDuration); err != nil {
+		return "", err
+	}
+
+	_ = s.resetStore.DeleteCode(ctx, id) //TODO: log error
+
+	return t, nil
+}
+
+func (s *Service) ValidateResetToken(ctx context.Context, token string) (string, error) {
+	_, err := s.tokenMaker.VerifyToken(token)
+	if err != nil {
+		return "", err
+	}
+
+	userID, err := s.resetStore.GetToken(ctx, token)
+	if err != nil {
+		return "", err
+	}
+
+	return userID, nil
+}
+
+func (s *Service) CompletePasswordReset(ctx context.Context, token, newPassword string) error {
+	userID, err := s.ValidateResetToken(ctx, token)
+	if err != nil {
+		return err
+	}
+	id, err := strconv.Atoi(userID)
+	if err != nil {
+		return err
+	}
+
+	hashedPassword, err := util.HashPassword(newPassword)
+	if err != nil {
+		return err
+	}
+
+	err = s.store.UpdatePassword(ctx, db.UpdatePasswordParams{
+		ID:           int32(id),
+		PasswordHash: hashedPassword,
+	})
+	if err != nil {
+		return err
+	}
+
+	_ = s.resetStore.DeleteToken(ctx, userID) //TODO: log
+	return nil
+}
+
+func (s *Service) generateResetCode() (string, error) {
+	if s.config.ResetCodeLength < 1 || s.config.ResetCodeLength > 9 {
+		return "", fmt.Errorf("invalid code length: %d", s.config.ResetCodeLength)
+	}
+
+	var buf [4]byte
+	if _, err := rand.Read(buf[:]); err != nil {
+		return "", fmt.Errorf("failed to generate code: %w", err)
+	}
+
+	num := binary.BigEndian.Uint32(buf[:])
+	maxValue := uint32(math.Pow10(s.config.ResetCodeLength))
+	code := num % maxValue
+
+	format := fmt.Sprintf("%%0%dd", s.config.ResetCodeLength)
+	return fmt.Sprintf(format, code), nil
 }
